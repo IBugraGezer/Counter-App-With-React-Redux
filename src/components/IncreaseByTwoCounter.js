2aca87a56aec775c65ddf81f504a138cb38c250d
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-23 10:01:38 +0300

    increase by two counter component connected to redux

--- src/components/IncreaseByTwoCounter.js
+++ src/components/IncreaseByTwoCounter.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { increaseByTwoCounter } from "../redux/counterActions";
+import { connect } from "react-redux";
 
-export default class IncreaseByTwoCounter extends Component {
+class IncreaseByTwoCounter extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <button
+          onClick={(e) => {
+            this.props.dispatch(increaseByTwoCounter());
+          }}
+        >
+          2 arttır
+        </button>
+      </div>
+    );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(increaseByTwoCounter, dispatch) };
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);

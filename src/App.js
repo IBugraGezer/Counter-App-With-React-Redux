6555b28bcb37bd9e673fb1824b3e7ceff429b934
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-23 10:12:51 +0300

    removed unnecessary files and imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Counter from "./components/Counter";
 import IncreaseCounter from "./components/IncreaseCounter";
 import DecreaseCounter from "./components/DecreaseCounter";

c2f91b3a1667b3980f5b3ad4e86b6b84f5dc5f5e
İbrahim Buğra Gezer <gezerbugra@gmail.com>
2021-06-23 09:02:10 +0300

    counter component connected to redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
-
-export default class Counter extends Component {
+import { connect } from "react-redux";
+class Counter extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>{this.props.counter}</h1>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return { counter: state.counterReducer };
+}
+
+export default connect(mapStateToProps)(Counter);
